a180a01a2ad2ae298a39b7f665a51f009502e590
itzisurii <itzIsurika08@gmail.com>
2026-06-10 13:48:50 +0530

    Add getUser method to UserController for retrieving user details

--- src/main/java/edu/iCET/controller/UserController.java
+++ src/main/java/edu/iCET/controller/UserController.java
@@ -1,5 +1,9 @@
 package edu.iCET.controller;
 
+import edu.iCET.model.dto.UserDTO;
+import edu.iCET.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    public UserDTO getUser(@PathVariable String username){
+
+        return userService.getUser(username);
+    }
 
 
 }
