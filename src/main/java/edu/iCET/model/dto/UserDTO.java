186743ebaa36c9f75661b48f9b3d4d479d3a488d
itzisurii <itzIsurika08@gmail.com>
2026-06-10 13:49:05 +0530

    Refactor UserDTO class to simplify structure and add toString method

--- src/main/java/edu/iCET/model/dto/UserDTO.java
+++ src/main/java/edu/iCET/model/dto/UserDTO.java
@@ -4,20 +4,14 @@ package edu.iCET.model.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UserDTO {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
 
     private String username;
     private String password;
