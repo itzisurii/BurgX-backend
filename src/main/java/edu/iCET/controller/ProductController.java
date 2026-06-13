0865cf063463d9e324134271571d5d517bccd7d6
itzisurii <itzIsurika08@gmail.com>
2026-06-14 00:57:28 +0530

    Add ProductController class with endpoint for loading products

--- src/main/java/edu/iCET/controller/ProductController.java
+++ src/main/java/edu/iCET/controller/ProductController.java
@@ -1,4 +1,22 @@
 package edu.iCET.controller;
 
+import edu.iCET.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public String loadProduct(){
+
+        return "Load Product Controller...";
+    }
+
 }
