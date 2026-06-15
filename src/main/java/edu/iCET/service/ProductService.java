57d9381a083959e03c8cb40aa62de391d88b0507
itzisurii <itzIsurika08@gmail.com>
2026-06-15 22:41:21 +0530

    Add method to retrieve all products in ProductService

--- src/main/java/edu/iCET/service/ProductService.java
+++ src/main/java/edu/iCET/service/ProductService.java
@@ -1,8 +1,14 @@
 package edu.iCET.service;
 
+import edu.iCET.model.dto.ProductDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
+    public List<ProductDTO> getAllProducts() {
+        return null;
+    }
 }
