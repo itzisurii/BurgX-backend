1f3c6218694fd33c39a1d64f1bfd12b96c66076f
itzisurii <itzIsurika08@gmail.com>
2026-08-06 21:37:52 +0530

    Add OrderController with endpoint to load orders and method to get last order ID

---
+++ src/main/java/edu/iCET/controller/OrderController.java
@@ -0,0 +1,28 @@
+package edu.iCET.controller;
+
+import edu.iCET.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping
+    public String loadOrder(){
+
+        return "Load Order Controller...";
+    }
+
+    public int getLastOrderId(){
+
+        return orderService.getLastOrderId();
+    }
+
+
+}
