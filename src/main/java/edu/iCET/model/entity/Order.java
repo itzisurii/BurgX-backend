6db4e514f647256245afc5ac53ebc4bca5dd18e6
itzisurii <itzIsurika08@gmail.com>
2026-08-16 20:28:54 +0530

    Enhance Order entity to include orderId, orderStatus, serviceType, totalValue, and a list of order items

--- src/main/java/edu/iCET/model/entity/Order.java
+++ src/main/java/edu/iCET/model/entity/Order.java
@@ -1,7 +1,12 @@
 package edu.iCET.model.entity;
 
+import edu.iCET.model.enums.OrderStatus;
+import edu.iCET.model.enums.ServiceType;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,12 +19,15 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long orderId;
 
-    private String productName;
-    private int quantity;
-    private double price;
-    private String customerName;
-    private String customerEmail;
-    private String customerAddress;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @Enumerated(EnumType.STRING)
+    private ServiceType serviceType;
+
+    private BigDecimal totalValue;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items;
 }
