3c9b993ec97e817a2dcef139ebdde0df09eb41d6
itzisurii <itzIsurika08@gmail.com>
2026-08-16 20:29:17 +0530

    Add addOrder method to OrderController for handling order creation

--- src/main/java/edu/iCET/controller/OrderController.java
+++ src/main/java/edu/iCET/controller/OrderController.java
@@ -1,8 +1,10 @@
 package edu.iCET.controller;
 
+import edu.iCET.model.dto.OrderDTO;
 import edu.iCET.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class OrderController {
         return orderService.getLastOrderId();
     }
 
+    public OrderDTO addOrder(@RequestBody OrderDTO order){
 
+        if (order != null){
+            return orderService.addOrder(order);
+        }
+        return null;
+    }
 
 }
